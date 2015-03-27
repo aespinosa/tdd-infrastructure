87823ad826a3319dd6964b22d9e5b13764fc2669
Allan Espinosa <allan.espinosa@outlook.com>
2015-03-27 21:20:02 +0800

    Add infrastructure orchestration for demo

---
+++ .chef/knife.rb
@@ -0,0 +1,6 @@
+node_name 'rubyconf'
+
+validation_key File.join(Dir.home, '.vagrant.d/insecure_private_key')
+client_key File.join(Dir.home, '.vagrant.d/insecure_private_key')
+
+chef_server_url 'http://chefserver.vm.dev:8889'
---
+++ vagrant.rb
@@ -0,0 +1,12 @@
+require 'chef/provisioning/vagrant_driver'
+
+with_driver 'vagrant'
+
+vagrant_box 'chef/freebsd10.1'
+
+cwd = File.expand_path(File.dirname __FILE__)
+vagrant_cluster File.join(cwd, '.chef/vms')
+
+machine 'hello.dev' do
+  action :destroy if ENV['reset']
+end
